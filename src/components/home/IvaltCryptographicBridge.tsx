b7d4a072bd2f7dc3e6711fe2c0b2a60902ececef
Jaskaran Singh <jaskaransingh4704@gmail.com>
2026-03-23 09:05:29 +0530

    Enhance IvaltCryptographicBridge component by adding icon for visual clarity and updating text layout for improved readability. Adjust styles for better alignment and consistency.

--- src/components/home/IvaltCryptographicBridge.tsx
+++ src/components/home/IvaltCryptographicBridge.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import IvaltCryptographicBridgeImage from '@/assets/images/home/ivalt-cryptographic-bridge.png';
+import { MoveRight } from 'lucide-react';
 
 const IvaltCryptographicBridge = () => {
   return (
@@ -7,8 +8,12 @@ const IvaltCryptographicBridge = () => {
       <h2 className="text-4xl md:text-5xl font-bold text-primary mb-6">
         iVALT is the Cryptographic Bridge
       </h2>
-      <p className="mx-auto mt-4 max-w-3xl text-xl font-bold italic text-teal-primary sm:text-2xl">
-        Existing Identity 🡪 Provable Human Authority
+      <p className="mx-auto mt-4 max-w-3xl text-xl font-bold italic text-teal-primary sm:text-3xl flex items-center justify-center gap-4">
+        <span>Existing Identity</span>
+        <span>
+          <MoveRight className="w-10 h-10" />
+        </span>{' '}
+        <span>Provable Human Authority</span>
       </p>
       <div className="max-w-7xl mx-auto flex justify-center py-6 md:py-12">
         {/* Main Image */}
@@ -20,8 +25,10 @@ const IvaltCryptographicBridge = () => {
           className="object-cover h-auto"
         />
       </div>
-      <p className="mx-auto mt-4 max-w-3xl text-xl font-bold italic text-primary sm:text-2xl">
-        Identity → Provable Authority = Trusted Execution
+      <p className="mx-auto mt-4 max-w-3xl text-xl font-bold italic text-primary sm:text-3xl flex items-center justify-center gap-4">
+        <span>Identity</span>
+        <MoveRight className="w-10 h-10" />
+        <span>Provable Authority</span> = <span>Trusted Execution</span>
       </p>
     </div>
   );
